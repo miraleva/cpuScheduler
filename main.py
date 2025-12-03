bfec7ae8de6ecef660ee1a3233226e0e1dd1153f
miraleva <miramirza74@gmail.com>
2025-12-03 23:47:58 +0300

    test rr

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from scheduler.utils.parser import parse_input
 from scheduler.algorithms.fcfs import FCFS
 from scheduler.algorithms.sjf import SJF
 from scheduler.algorithms.priority import PriorityScheduler
+from scheduler.algorithms.round_robin import RoundRobin
 
 if len(sys.argv) < 3:
     print("Usage: python main.py <input_file> <algorithm> [time_quantum_for_RR]")
@@ -22,6 +23,11 @@ elif algorithm == "sjf":
     scheduler = SJF()
 elif algorithm == "priority":
     scheduler = PriorityScheduler()
+elif algorithm == "rr":
+    if time_quantum is None:
+        print("Error: Time quantum is required for Round Robin.")
+        sys.exit(1)
+    scheduler = RoundRobin(time_quantum)
 else:
     print(f"Unknown algorithm: {algorithm}")
     sys.exit(1)
